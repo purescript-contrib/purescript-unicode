ac0ecf2ac2f20c73940b327dafeb67556db119b5
Thomas Honeyman <admin@thomashoneyman.com>
2020-10-06 09:18:29 -0700

    Disable escape character tests

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -6,7 +6,7 @@ import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Test.Data.Char.Unicode (dataCharUnicodeTests)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (run, runSpec)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
